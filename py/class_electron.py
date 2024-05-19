bda946ddbb4d638db95ac185833c63d0fd366506
Adrian <kerpador@gmail.com>
2024-05-18 23:17:28 -0600

    add new functions

--- py/class_electron.py
+++ py/class_electron.py
@@ -32,6 +32,22 @@ class electron:
         if m>l:
             raise ValueError("l and m must be postive, it means, that l can't be smaller than m")
         return R,phi,theta,fcolors,l,m
+    def ImaginarySpherical(self,l,m):
+       #Creade grid of phi and theta angles for ploting surface mesh
+        phi, theta = np.linspace(0, np.pi, 100), np.linspace(0, 2*np.pi, 100)
+        phi, theta = np.meshgrid(phi, theta)
+
+        #Calcualte spherical harmonic with given m and l
+        Ylm = sph_harm(m, l, theta, phi)
+        R=abs(Ylm)
+
+        # Let's normalize color scale
+        fcolors    = Ylm.imag
+        fmax, fmin = fcolors.max(), fcolors.min()
+        fcolors    = (fcolors - fmin)/(fmax - fmin)
+        if m>l:
+                raise ValueError("l and m must be postive, it means, that l can't be smaller than m")
+        return R,phi,theta,fcolors,l,m 
 class plot:
     def __init__(self, *args):
         super(plot, self).__init__(*args)
@@ -82,4 +98,40 @@ class plot:
                             font=dict(
                             color="white"))
             fig.show()
-                
+    def plot_spherical_imaginary(self,package=None,R=None,theta=None,phi=None,fcolors=None,l=None,m=None):
+        if package:
+            fig = go.Figure(data=[go.Surface(x=package[0]*np.sin(package[1]) * np.cos(package[2]),
+                                     y=package[0]*np.sin(package[1]) * np.sin(package[2]),
+                                     z=package[0]*np.cos(package[1]),
+                                     surfacecolor=package[3],
+                                     colorscale='balance')])
+
+    # Show the plot
+            fig.update_layout(title=fr'$Y_{package[4], package[5]}$', autosize=False,
+                            width=700, height=700,
+                            margin=dict(l=65, r=50, b=65, t=90),paper_bgcolor="black",
+                            scene=dict(
+                            xaxis=dict(visible=False),
+                            yaxis=dict(visible=False),
+                            zaxis=dict(visible=False)),
+                            font=dict(
+                            color="white"))
+            fig.show()
+        else:
+            fig = go.Figure(data=[go.Surface(x=R*np.sin(phi) * np.cos(theta),
+                                     y=R*np.sin(phi) * np.sin(theta),
+                                     z=R*np.cos(phi),
+                                     surfacecolor=fcolors,
+                                     colorscale='balance')])
+
+    # Show the plot
+            fig.update_layout(title=fr'$Y_{l, m}$', autosize=False,
+                            width=700, height=700,
+                            margin=dict(l=65, r=50, b=65, t=90),paper_bgcolor="black",
+                            scene=dict(
+                            xaxis=dict(visible=False),
+                            yaxis=dict(visible=False),
+                            zaxis=dict(visible=False)),
+                            font=dict(
+                            color="white"))
+            fig.show()
